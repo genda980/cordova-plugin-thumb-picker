1ab99df8752169ca0fe148cd0f6b8d0faa31d6be
genda <gendaliu@yeah.net>
2021-05-18 00:36:32 +0800

    test

--- www/ThumbPicker.js
+++ www/ThumbPicker.js
@@ -3,3 +3,8 @@ var exec = require('cordova/exec');
 exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'ThumbPicker', 'coolMethod', [arg0]);
 };
+
+
+exports.coolMethod1 = function (arg0, success, error) {
+    exec(success, error, 'ThumbPicker', 'coolMethod1', [arg0]);
+};
